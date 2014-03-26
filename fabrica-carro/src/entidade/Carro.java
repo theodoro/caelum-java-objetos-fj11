8a4d65754965e91d63c57ea7af76a9d908bf21c1
Bruno Aquino <theodorodeaquino@gmail.com>
2014-03-25 22:04:30 -0300

    Exercicio Fabrica de carros

---
+++ fabrica-carro/src/entidade/Carro.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entidade;
+
+/**
+ *
+ * @author brunoaquino
+ */
+public class Carro {
+    
+   public String cor;
+   public String modelo;
+   public double velocidadeAtual;
+   public double velocidadeMax;
+    
+    
+    public void ligar(){
+        System.out.println("O Carro esta ligado!");
+        
+    }
+    
+    public void acelera(double qtde){
+        double novaVelocidade = this.velocidadeAtual + qtde;
+        this.velocidadeAtual = novaVelocidade;
+    }
+    
+    public int getMarcha(){
+        if(this.velocidadeAtual < 0){
+            return -1;
+        }if(this.velocidadeAtual >= 0 && this.velocidadeAtual < 40){
+            return 1;
+        }if(this.velocidadeAtual >= 40 && this.velocidadeAtual < 80){
+            return 2;
+        }else{
+            return 3;
+        }
+    }
+    
+}
---
+++ fabrica-carro/src/start/Programa.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package start;
+
+import entidade.Carro;
+
+/**
+ *
+ * @author brunoaquino
+ */
+public class Programa {
+    
+    public static void main(String[] args){
+        
+        Carro fusca = new Carro();
+        
+        fusca.cor = "Preto";
+        fusca.modelo = "1600";
+        fusca.velocidadeAtual = 50;
+        fusca.velocidadeMax = 100;
+        
+        fusca.ligar();
+        
+        fusca.acelera(90);
+        
+        fusca.getMarcha();
+        
+        System.out.println(fusca.velocidadeAtual);
+    }
+    
+}
